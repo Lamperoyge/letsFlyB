00dd4c309d2aa7aab61b979ccff5fa388de1c782
Adrian Mustea <mustea.adrian95@gmail.com>
2018-09-21 00:57:01 +0300

    card

--- client/Components/App.js
+++ client/Components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import {render} from 'react-dom'
 import Navbar from './Navbar'
+import CardIndexPage from './Card'
 export default class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <CardIndexPage />
       </div>
     )
   }
---
+++ client/Components/Card.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+export default class CardIndexPage extends Component {
+  render() {
+    return (
+        <div className = "card-container">
+          <div>
+            <div className = "image">
+                
+            </div>
+            <div className = "form">
+                    
+            </div>
+          </div>
+        </div>
+    )
+  }
+}
